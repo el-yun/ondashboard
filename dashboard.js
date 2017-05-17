dc3d7ed13fc5f3b56d5ff7c8a806ff4eed8f81e8
yoonjihwan <sac5@naver.com>
2017-05-17 15:02:40 +0900

    aaaaa

--- dashboard.js
+++ dashboard.js
@@ -3,33 +3,101 @@ var children = [
 	{num: 2, name: '테스트2', comment: '두번째'}
 ];
 
+var children2 =  [
+	{num: 3, name: '테스트3', comment: '세번째'}, 
+	{num: 4, name: '테스트4', comment: '네번째'}
+];
+
 /*
-	Table Listing
+	Dashboard lib
 */
+(function(window){
 
-function addHeaders(table, keys) {
-  var row = table.insertRow();
-  for( var i = 0; i < keys.length; i++ ) {
-    var cell = row.insertCell();
-    cell.appendChild(document.createTextNode(keys[i]));
-  }
-}
-function Dashboard(id, children){
-	this.table = document.createElement(id);
-	for( var i = 0; i < children.length; i++ ) {
-	  var child = children[i];
-	  if(i === 0 ) {
-		addHeaders(table, Object.keys(child));
-	  }
-	  var row = table.insertRow();
-	  Object.keys(child).forEach(function(k) {
-		console.log(k);
-		var cell = row.insertCell();
-		cell.appendChild(document.createTextNode(child[k]));
-	  })
+	// Table
+	var Table = function(_table){
+		this._object = _table;
 	}
-}
 
-Dashboard.prototype.render = function(target){
-	return document.getElementById(target).appendChild(table);
-}
+	Table.prototype.addObject = function(c){
+		for( var i = 0; i < c.length; i++ ) {
+		  var child = c[i];
+		  var row = this._object.insertRow();
+		  Object.keys(child).forEach(function(k) {
+			console.log(k);
+			var cell = row.insertCell();
+			cell.appendChild(document.createTextNode(child[k]));
+		  })
+		}
+		return this;
+	}
+
+	Table.prototype.addHeaders = function(keys) {
+	  var row = this._object.insertRow();
+	  for( var i = 0; i < keys.length; i++ ) {
+		var cell = row.insertCell();
+		cell.appendChild(document.createTextNode(keys[i]));
+	  }
+	}
+
+	Table.prototype.render = function(target){
+		document.getElementById(target).innerHTML = "";
+		document.getElementById(target).append(this._object);
+	}
+
+	// Base
+	var Dashboard = {
+		__obj : null,
+		makeTable : function(id){
+			var table = new Table(document.createElement(id));
+
+			for( var i = 0; i < children.length; i++ ) {
+			  var child = children[i];
+			  if(i === 0 ) {
+				table.addHeaders(Object.keys(child));
+			  }
+			  var row = table._object.insertRow();
+			  Object.keys(child).forEach(function(k) {
+				var cell = row.insertCell();
+				cell.appendChild(document.createTextNode(child[k]));
+			  })
+			}
+			  console.log(table);
+			return table;
+		}
+	}
+
+	// Export
+	window.Dashboard = Dashboard;
+
+})(window);
+
+
+	function ajax(url, method, data, async)
+	{
+		method = typeof method !== 'undefined' ? method : 'GET';
+		async = typeof async !== 'undefined' ? async : false;
+
+		if (window.XMLHttpRequest) {
+			var xhReq = new XMLHttpRequest();
+		}
+		else {
+			var xhReq = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+
+
+		if (method == 'POST'){
+			xhReq.open(method, url, async);
+			xhReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			xhReq.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+			xhReq.send(data);
+		}
+		else
+		{
+			if(typeof data !== 'undefined' && data !== null) {
+				url = url+'?'+data;
+			}
+			xhReq.open(method, url, async);
+			xhReq.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+			xhReq.send(null);
+		}
+	}
